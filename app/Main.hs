c14ef3c3bf9a040c3b854f85704a31cf8ae89a62
MeahTheCat could be a girl <affanmeah@gmail.com>
2026-02-27 10:48:24 +0000

    TKT

---
+++ app/Main.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import System.Environment 
+import Text.Read (readMaybe)
+import Data.List 
+import System.IO (readFile)
+
+split :: Char -> String -> [String]
+split _ [] = []
+split c s = firstWord : (split c rest)
+  where firstWord = takeWhile (/= c) s
+        rest = dropWhile (== c) (drop (length firstWord) s)
+
+speedUp :: Float -> Float -> Float
+speedUp ts tp = ts / tp
+
+efficiency :: Float -> Float -> Float
+efficiency ts tp = (speedUp ts tp) / ts
+
+--Csv Parser
+parseLine :: String -> (Int, Int, Float, Float, Float)
+parseLine line = 
+    let [input, cores, run1, run2, run3] = map read (split ',' line)
+    in (input, cores, run1, run2, run3)
+
+--File reader
+processFile :: String -> IO ()
+processFile fileName = do
+    content <- readFile fileName
+    let linesOfFile = lines content
+        parsedData = map parseLine (tail linesOfFile)  -- Ignore header
+        results = map (\(input, cores, run1, run2, run3) -> 
+                        let avgSeq = (run1 + run2 + run3) / 3
+                            speedup = speedUp avgSeq (fromIntegral cores * avgSeq)
+                            eff = efficiency avgSeq (fromIntegral cores * avgSeq)
+                        in (input, cores, avgSeq, speedup, eff)) parsedData
+        newCSV = "input,cores,avgRun,speedup,efficiency\n" ++ 
+                 unlines (map (\(i, c, avg, sp, eff) -> 
+                    intercalate "," [show i, show c, show avg, show sp, show eff]) results)
+    writeFile "output.csv" newCSV
+
+-- Main
+main :: IO ()
+main = do
+    args <- getArgs
+    let parallelRuntimeFile = case args of
+            [f] -> f
+            _ -> error "Too much or not enough args/n"
+    processFile parallelRuntimeFile
